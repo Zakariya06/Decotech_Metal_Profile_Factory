f022eae57a2d60319d21a14a9fdaff745a439638
Zakariya06 <zakariyakhanamar00@gmail.com>
2025-02-08 21:04:32 +0500

    add all the text

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-// Swiper initialization
-const swiper = new Swiper(".productSwiper", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".swiperNextBtn",
-    prevEl: ".swiperPrevBtn",
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-    1280: {
-      slidesPerView: 6,
-    },
-  },
-});
 
 // side menu
 const hamburger = document.querySelector("#hamburger");
